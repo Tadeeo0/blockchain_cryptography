ecf29b70765656eb6eb1becb984179ad04d675ec
Tadeo <diego.tadeo4922@alumnos.udg.mx>
2025-05-18 17:01:48 -0600

    Add notes.

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ bp = Blueprint('main', __name__)
 def home():
     return render_template('login.html')
 
+#Registro
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -19,13 +20,15 @@ def register():
         flash('Registro exitoso. Ahora puedes iniciar sesión.', 'success')
         return redirect(url_for('main.home'))
     
-    # Si es GET, solo mostrar el formulario
+
     return render_template('register.html')
 
+#Dash principal
 @bp.route('/dashboard', methods=['GET', 'POST'])
 def home_view():
     return render_template('home_view.html')
 
+#Cerrar sesion
 @bp.route('/logout', methods=['POST'])
 def logout():
     return redirect(url_for('main.home'))
