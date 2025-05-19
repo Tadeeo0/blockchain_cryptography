ee89d3035638469dc2e4c01f28a159cb4728c02b
DinaxisLF <cesar5342@hotmail.com>
2025-05-19 15:09:26 -0600

    Integrate Backend API Calls

--- app/routes.py
+++ app/routes.py
@@ -37,19 +37,7 @@ def logout():
 # Ruta para historial de tokens
 @bp.route('/historial')
 def mostrar_historial():
-    historial_ficticio = [
-        {'fecha': '30-04-2025', 'tipo': 'Cartón', 'cantidad': '2.0 kg', 'tokens': 20},
-        {'fecha': '01-05-2025', 'tipo': 'Vidrio', 'cantidad': '0.8 kg', 'tokens': 8},
-        {'fecha': '03-05-2025', 'tipo': 'Lata', 'cantidad': '1.5 kg', 'tokens': 15},
-        {'fecha': '03-05-2025', 'tipo': 'Cartón', 'cantidad': '2.0 kg', 'tokens': 20},
-        {'fecha': '06-05-2025', 'tipo': 'Plastico', 'cantidad': '2.0 kg', 'tokens': 20},
-        {'fecha': '06-05-2025', 'tipo': 'Vidrio', 'cantidad': '0.8 kg', 'tokens': 8},
-        {'fecha': '09-05-2025', 'tipo': 'Vidrio', 'cantidad': '0.8 kg', 'tokens': 8},
-        {'fecha': '13-05-2025', 'tipo': 'Cartón', 'cantidad': '2.0 kg', 'tokens': 20},
-        {'fecha': '16-05-2025', 'tipo': 'Lata', 'cantidad': '1.5 kg', 'tokens': 15}
-    ]
-    total = sum(item['tokens'] for item in historial_ficticio)
-    return render_template('historial.html', historial=historial_ficticio, total_tokens=total)
+    return render_template('historial.html')
 
 #Ruta para intercambiar tokens
 @bp.route('/canjear')
